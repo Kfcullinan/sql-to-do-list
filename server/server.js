49b3733a6d7b2af79bfe4634af9b00cab266d032
Keith Cullinan <kfcullinan@gmail.com>
2022-08-13 13:58:54 -0500

    added in server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5003;
+
+app.use(express.static('server/public'));
+
+app.listen(port, () => {
+console.log('listening on port', port)
+});
